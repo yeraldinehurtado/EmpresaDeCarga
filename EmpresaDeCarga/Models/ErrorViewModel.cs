6b236eba74c82c18e59fac60c4bc0912b17f6eb4
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-05-28 14:45:22 -0500

    Agregar archivos de proyecto.

---
+++ EmpresaDeCarga/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmpresaDeCarga.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
