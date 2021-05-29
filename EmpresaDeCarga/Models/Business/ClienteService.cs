5136980b77c915cf006eaaf72e116a2c11c8f4ef
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-05-28 22:01:58 -0500

    cambio en clienteService

--- EmpresaDeCarga/Models/Business/ClienteService.cs
+++ EmpresaDeCarga/Models/Business/ClienteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmpresaDeCarga.Models.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace EmpresaDeCarga.Models.Business
 {
     public class ClienteService
     {
+        private readonly AppDbContext _context;
+        public ClienteService(AppDbContext context)
+        {
+            _context = context;
+        }
     }
 }
