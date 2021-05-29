374d4dc1812f477d627a41a6301eed3b628d38c8
Yeraldine Hurtado Muñoz <yeraldinehurtado1@gmail.com>
2021-05-28 19:32:37 -0500

    Cambio en Startup.cs y appsettings.json

--- EmpresaDeCarga/Startup.cs
+++ EmpresaDeCarga/Startup.cs
@@ -1,6 +1,8 @@
+using EmpresaDeCarga.Models.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace EmpresaDeCarga
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            var conexion = Configuration["ConnectionStrings:conexion_sqlServer"];
+            services.AddDbContext<AppDbContext>(options => 
+                        options.UseSqlServer(conexion));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
